cec8c8c5e7af1b156179bde1cc8abc5b800ac466
javierrm92 <160373709+javierrm92@users.noreply.github.com>
2024-05-07 02:02:48 -0600

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "RG-IacGit"
-    storage_account_name = "storagegitiac001 "
+    storage_account_name = "storagegitiac003"
     container_name       = "containeractions"
     key                  = "stateActions.tfstate"
   }
